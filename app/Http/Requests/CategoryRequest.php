39f3055e9471c11309cff21f7a72aaaea5f9532d
Arti Gupta <160204792+Artidev08@users.noreply.github.com>
2025-03-27 12:11:31 +0530

    Fix

--- app/Http/Requests/CategoryRequest.php
+++ app/Http/Requests/CategoryRequest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Http\FormRequest;
 class CategoryRequest extends FormRequest
 {
     /**
+     * change
      * Determine if the user is authorized to make this request.
      *
      * @return bool
